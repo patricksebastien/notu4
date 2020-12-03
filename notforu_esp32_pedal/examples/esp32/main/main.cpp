0296a176ec57c73e030b4869413dca89c7826534
Patrick <patrick@agora.studio>
2020-12-02 23:29:51 -0500

    clean_up

---
+++ notforu_esp32_pedal/examples/esp32/main/main.cpp
@@ -0,0 +1,430 @@
+#include <ableton/Link.hpp>
+#include <driver/gpio.h>
+#include <driver/timer.h>
+#include <esp_event.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <freertos/task.h>
+#include <nvs_flash.h>
+#include <protocol_examples_common.h>
+#include "driver/uart.h"
+#include <stdio.h>
+#include "esp_timer.h"
+#include "esp_sleep.h"
+
+
+// UPD
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "tcpip_adapter.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+#define HOST_IP_ADDR "192.168.2.101"
+#define PORT 3333
+
+static const char *TAG = "example";
+static const char *payload = "Message from ESP32 ";
+
+char rx_buffer[128];
+char addr_str[128];
+int addr_family;
+int ip_protocol;
+struct sockaddr_in dest_addr;
+int sock;
+
+#define LED GPIO_NUM_2
+#define PRINT_LINK_STATE false
+
+// Serial midi
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_TXD  (GPIO_NUM_4)
+#define ECHO_TEST_RXD  (GPIO_NUM_5)
+#define BUF_SIZE (1024)
+#define MIDI_TIMING_CLOCK 0xF8
+#define MIDI_START 0xFA
+#define MIDI_STOP 0xFC
+#define MIDI_SONG_POSITION_POINTER 0xF2
+
+// Global
+static void periodic_timer_callback(void* arg);
+esp_timer_handle_t periodic_timer;
+bool startStopCB = false;
+bool startStopState = false;
+double curr_beat_time;
+double prev_beat_time;
+
+// gpio
+extern "C" {
+  #include "freertos/queue.h"
+  #include <string.h>
+  #include <stdlib.h>
+
+  // BUTTONS
+  #define GPIO_INPUT_IO_0     GPIO_NUM_23
+  #define GPIO_INPUT_IO_1     GPIO_NUM_1
+  #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+
+  // BUTTONS LED
+  #define GPIO_OUTPUT_IO_0    GPIO_NUM_18
+  #define GPIO_OUTPUT_IO_1    GPIO_NUM_19
+  #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
+  #define ESP_INTR_FLAG_DEFAULT 0
+
+  static xQueueHandle gpio_evt_queue = NULL;
+
+  static void IRAM_ATTR gpio_isr_handler(void* arg)
+  {
+      uint32_t gpio_num = (uint32_t) arg;
+      xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+  }
+
+  static void gpio_task_example(void* arg)
+  {
+      uint32_t io_num;
+      for(;;) {
+          if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+              
+              if(io_num == GPIO_NUM_23 && gpio_get_level((gpio_num_t)io_num) == 0) {
+                payload = "23";
+                //gpio_set_level(GPIO_OUTPUT_IO_0, 1);
+              } else if(io_num == GPIO_NUM_1 && gpio_get_level((gpio_num_t)io_num) == 0) {
+                payload = "1";
+                //gpio_set_level(GPIO_OUTPUT_IO_0, 0);
+              }
+//              gpio_set_level(GPIO_OUTPUT_IO_0, gpio_get_level((gpio_num_t)io_num));
+
+              int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+                  if (err < 0) {
+                      ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                      break;
+                  }
+                  ESP_LOGI(TAG, "Message sent");
+                
+                printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level((gpio_num_t)io_num));
+          }
+      }
+  }
+}
+
+
+unsigned int if_nametoindex(const char* ifName)
+{
+  return 0;
+}
+
+char* if_indextoname(unsigned int ifIndex, char* ifName)
+{
+  return nullptr;
+}
+
+void IRAM_ATTR timer_group0_isr(void* userParam)
+{
+  static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+  TIMERG0.int_clr_timers.t0 = 1;
+  TIMERG0.hw_timer[0].config.alarm_en = 1;
+
+  xSemaphoreGiveFromISR(userParam, &xHigherPriorityTaskWoken);
+  if (xHigherPriorityTaskWoken)
+  {
+    portYIELD_FROM_ISR();
+  }
+}
+
+void timerGroup0Init(int timerPeriodUS, void* userParam)
+{
+  timer_config_t config = {.alarm_en = TIMER_ALARM_EN,
+    .counter_en = TIMER_PAUSE,
+    .intr_type = TIMER_INTR_LEVEL,
+    .counter_dir = TIMER_COUNT_UP,
+    .auto_reload = TIMER_AUTORELOAD_EN,
+    .divider = 80};
+
+  timer_init(TIMER_GROUP_0, TIMER_0, &config);
+  timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
+  timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, timerPeriodUS);
+  timer_enable_intr(TIMER_GROUP_0, TIMER_0);
+  timer_isr_register(TIMER_GROUP_0, TIMER_0, &timer_group0_isr, userParam, 0, nullptr);
+
+  timer_start(TIMER_GROUP_0, TIMER_0);
+}
+
+void printTask(void* userParam)
+{
+  auto link = static_cast<ableton::Link*>(userParam);
+  const auto quantum = 4.0;
+
+  while (true)
+  {
+    const auto sessionState = link->captureAppSessionState();
+    const auto numPeers = link->numPeers();
+    const auto time = link->clock().micros();
+    const auto beats = sessionState.beatAtTime(time, quantum);
+    std::cout << std::defaultfloat << "| peers: " << numPeers << " | "
+              << "tempo: " << sessionState.tempo() << " | " << std::fixed
+              << "beats: " << beats << " |" << std::endl;
+    vTaskDelay(800 / portTICK_PERIOD_MS);
+  }
+}
+
+// callbacks
+void tempoChanged(double tempo) {
+    double midiClockMicroSecond = ((60000 / tempo) / 24) * 1000;
+    esp_timer_handle_t periodic_timer_handle = (esp_timer_handle_t) periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_stop(periodic_timer_handle));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer_handle, midiClockMicroSecond));
+}
+
+void startStopChanged(bool state) {
+  // received as soon as sent
+  // need to wait for phase to be 0 (and deal with latency...)
+  startStopCB = state;
+}
+
+void tickTask(void* userParam)
+{
+  // connect link
+  ableton::Link link(120.0f);
+  link.enable(true);
+  link.enableStartStopSync(true); // if not no callback for start/stop
+
+  // callbacks
+  link.setTempoCallback(tempoChanged);
+  link.setStartStopCallback(startStopChanged);
+
+  // debug
+  if (PRINT_LINK_STATE)
+  {
+    xTaskCreate(printTask, "print", 8192, &link, 1, nullptr);
+  }
+  gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+
+  // phase
+  while (true)
+  { 
+    xSemaphoreTake(userParam, portMAX_DELAY);
+
+    const auto state = link.captureAudioSessionState();
+    const auto phase = state.phaseAtTime(link.clock().micros(), 0.25);
+    gpio_set_level(LED, fmodf(phase, 1.) < 0.1);
+   
+
+
+
+    curr_beat_time = state.beatAtTime(link.clock().micros(), 4);
+    const double curr_phase = fmod(curr_beat_time, 4);
+    if (curr_beat_time > prev_beat_time) {
+      const double prev_phase = fmod(prev_beat_time, 4);
+      const double prev_step = floor(prev_phase * 1);
+      const double curr_step = floor(curr_phase * 1);
+      if (prev_phase - curr_phase > 4 / 2 || prev_step != curr_step) {
+        if(curr_step ==0) {
+          gpio_set_level(GPIO_OUTPUT_IO_0, 1);
+        } else {
+          gpio_set_level(GPIO_OUTPUT_IO_0, 0);
+        }
+        if(curr_step == 0 && startStopState != startStopCB) {
+              if(startStopCB) {
+                char zedata[] = { MIDI_START };
+                uart_write_bytes(UART_NUM_1, zedata, 1);
+                uart_write_bytes(UART_NUM_1, 0, 1);
+              } else {
+                char zedata[] = { MIDI_STOP };
+                uart_write_bytes(UART_NUM_1, zedata, 1);
+                uart_write_bytes(UART_NUM_1, 0, 1);
+              }
+              startStopState = startStopCB;
+        }
+        
+      }
+    }
+    prev_beat_time = curr_beat_time;
+
+    portYIELD();
+  }
+}
+
+static void periodic_timer_callback(void* arg)
+{
+    char zedata[] = { MIDI_TIMING_CLOCK };
+    uart_write_bytes(UART_NUM_1, zedata, 1);
+}
+
+
+
+// UPD
+static void udp_client_task(void *pvParameters)
+{
+    while (1) {
+
+        while (1) {
+
+            int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+            if (err < 0) {
+                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                break;
+            }
+            ESP_LOGI(TAG, "Message sent");
+
+            struct sockaddr_in source_addr; // Large enough for both IPv4 or IPv6
+            socklen_t socklen = sizeof(source_addr);
+            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+
+            // Error occurred during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                break;
+            }
+            // Data received
+            else {
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+                ESP_LOGI(TAG, "%s", rx_buffer);
+            }
+
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+extern "C" void app_main()
+{
+  ESP_ERROR_CHECK(nvs_flash_init());
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  ESP_ERROR_CHECK(example_connect());
+
+  // serial
+  uart_config_t uart_config = {
+    .baud_rate = 31250, // midi speed
+    .data_bits = UART_DATA_8_BITS,
+    .parity    = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    .rx_flow_ctrl_thresh = 122,
+  };
+  uart_param_config(UART_NUM_1, &uart_config);
+  uart_set_pin(UART_NUM_1, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+  uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
+
+  // upd init + timer
+  dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_port = htons(PORT);
+  addr_family = AF_INET;
+  ip_protocol = IPPROTO_IP;
+  inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
+  sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+  if (sock < 0) {
+      ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+  }
+  ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT);
+  xTaskCreate(udp_client_task, "udp_client", 4096, NULL, 5, NULL);
+
+  // link timer - phase
+  SemaphoreHandle_t tickSemphr = xSemaphoreCreateBinary();
+  timerGroup0Init(1000, tickSemphr);
+  xTaskCreate(tickTask, "tick", 8192, tickSemphr, 1, nullptr);
+
+  // midi clock
+  const esp_timer_create_args_t periodic_timer_args = {
+          .callback = &periodic_timer_callback,
+          .name = "periodic"
+  };
+  ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+  ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 20833.333333333)); // 120 bpm by default
+
+
+/*
+    //disable interrupt
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode    
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio intrrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+
+    //remove isr handler for gpio number.
+    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);*/
+
+  // gpio
+  gpio_config_t io_conf;   // GPIO
+
+      //disable interrupt
+    io_conf.intr_type = (gpio_int_type_t)GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = (gpio_pulldown_t)0;
+    //disable pull-up mode
+    io_conf.pull_up_en = (gpio_pullup_t)0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+
+  io_conf.intr_type = (gpio_int_type_t)GPIO_PIN_INTR_NEGEDGE; 
+  io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;  //bit mask of the pins, use GPIO4
+  io_conf.mode = GPIO_MODE_INPUT;  //set as input mode   
+  io_conf.pull_down_en = (gpio_pulldown_t)0; // 0 //disable pull-down mode
+  io_conf.pull_up_en = (gpio_pullup_t)1; //enable pull-up mode
+  gpio_config(&io_conf);
+
+  gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t)); //create a queue to handle gpio event from isr
+  xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL); //start gpio task
+
+      //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+}
---
+++ notforu_teensy_synth/notforu.ino
@@ -0,0 +1,382 @@
+#include <ResponsiveAnalogRead.h>
+#include <Bounce.h>
+
+// OLED
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins) - pin 17-18 (A3-A4)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+// the MIDI channel number to send messages
+const int channel = 1;
+const int cchigh = 127;
+const int cclow = 0;
+
+int isRec = 0;
+int isDub = 0;
+
+ResponsiveAnalogRead a0(A0, true);
+ResponsiveAnalogRead a1(A1, true);
+ResponsiveAnalogRead a2(A2, true);
+ResponsiveAnalogRead a3(A3, true);
+ResponsiveAnalogRead a6(A6, true);
+ResponsiveAnalogRead a7(A7, true);
+ResponsiveAnalogRead a8(A8, true);
+ResponsiveAnalogRead a9(A9, true);
+ResponsiveAnalogRead a14(A14, true);
+int previousA0 = -1;
+int previousA1 = -1;
+int previousA2 = -1;
+int previousA3 = -1;
+int previousA6 = -1;
+int previousA7 = -1;
+int previousA8 = -1;
+int previousA9 = -1;
+int previousA14 = -1;
+
+
+// Create Bounce objects for each button.  The Bounce object
+// automatically deals with contact chatter or "bounce", and
+// it makes detecting changes very simple.
+Bounce button1 = Bounce(1, 5);  // 5 = 5 ms debounce time
+Bounce button2 = Bounce(2, 5);  // which is appropriate for good
+Bounce button3 = Bounce(3, 5);  // quality mechanical pushbuttons
+Bounce button4 = Bounce(4, 5);
+Bounce button5 = Bounce(5, 5);  // if a button is too "sensitive"
+Bounce button6 = Bounce(6, 5);  // to rapid touch, you can
+Bounce button7 = Bounce(7, 5);  // increase this time.
+Bounce button8 = Bounce(8, 5);
+Bounce button9 = Bounce(9, 5);
+Bounce button10 = Bounce(10, 5);
+Bounce button12 = Bounce(12, 5);
+
+// countdown
+#define COUNTDOWN_TIME  60000ul
+
+// n14
+String line1;
+String line2;
+
+char
+    szString[20];
+byte
+    mins, secs;
+       
+unsigned long
+    timeTemp,
+    timeNow,
+    timeStart,
+    timeElapsed,
+    timeLeft;
+
+bool countDownDone = 0;
+
+void setup() {
+
+  //OLED
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  pinMode(0, OUTPUT);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  digitalWrite(0, LOW);
+
+  // countdown
+  timeStart = millis();
+  mins = 1;
+  secs = 1;
+  
+  jasSplash();    // Draw 'stylized' characters
+}
+
+void jasSplash(void) {
+  display.clearDisplay();
+
+  display.setTextSize(6);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("JAS"));
+  display.display();
+  delay(2); // set as long as soft is not ready (20000)?
+}
+
+void loop() {
+  
+  // ANALOG
+  a0.update();
+  a1.update();
+  a2.update();
+  a3.update();
+  a6.update();
+  a7.update();
+  a8.update();
+  a9.update();
+  a14.update();
+  int n0 = a0.getValue() / 8;
+  if (n0 != previousA0) {
+    usbMIDI.sendControlChange(20, n0, channel);
+    previousA0 = n0;
+  }
+  int n1 = a1.getValue() / 8;
+  if (n1 != previousA1) {
+    usbMIDI.sendControlChange(21, n1, channel);
+    previousA1 = n1;
+  }
+  int n2 = a2.getValue() / 8;
+  if (n2 != previousA2) {
+    usbMIDI.sendControlChange(22, n2, channel);
+    previousA2 = n2;
+  }
+  int n3 = a3.getValue() / 8;
+  if (n3 != previousA3) {
+    usbMIDI.sendControlChange(25, n3, channel);
+    previousA3 = n3;
+  }
+  int n6 = a6.getValue() / 8;
+  if (n6 != previousA6) {
+    usbMIDI.sendControlChange(26, n6, channel);
+    previousA6 = n6;
+  }
+  int n7 = a7.getValue() / 8;
+  if (n7 != previousA7) {
+    usbMIDI.sendControlChange(27, n7, channel);
+    previousA7 = n7;
+  }
+  int n8 = a8.getValue() / 8;
+  if (n8 != previousA8) {
+    usbMIDI.sendControlChange(28, n8, channel);
+    previousA8 = n8;
+  }
+  int n9 = a9.getValue() / 8;
+  if (n9 != previousA9) {
+    usbMIDI.sendControlChange(29, n9, channel);
+    previousA9 = n9;
+  }
+  int n14 = a14.getValue() / 8;
+  if (n14 != previousA14) {
+    usbMIDI.sendControlChange(23, n14, channel);
+    previousA14 = n14;
+  }
+
+  //DIGITAL
+  button1.update();
+  button2.update();
+  button3.update();
+  button4.update();
+  button5.update();
+  button6.update();
+  button7.update();
+  button8.update();
+  button9.update();
+  button10.update();
+  button12.update();
+
+  if (button1.fallingEdge()) {
+    usbMIDI.sendControlChange(1, cclow, channel);
+  }
+  if (button2.fallingEdge()) {
+    usbMIDI.sendControlChange(2, cclow, channel);
+  }
+  if (button3.fallingEdge()) {
+    usbMIDI.sendControlChange(3, cclow, channel);
+  }
+  if (button4.fallingEdge()) {
+    usbMIDI.sendControlChange(4, cclow, channel);
+  }
+  if (button5.fallingEdge()) {
+    usbMIDI.sendControlChange(5, cclow, channel);
+  }
+  if (button6.fallingEdge()) {
+    usbMIDI.sendControlChange(6, cclow, channel);
+  }
+  if (button7.fallingEdge()) {
+    usbMIDI.sendControlChange(14, cclow, channel);
+    if(isDub == 0) {
+      isDub = 1;
+      digitalWrite(0, HIGH);
+    } else {
+      isDub = 0;
+      digitalWrite(0, LOW);
+    }
+  }
+  if (button8.fallingEdge()) {
+    usbMIDI.sendControlChange(8, cclow, channel);
+  }
+  if (button9.fallingEdge()) {
+    usbMIDI.sendControlChange(9, cclow, channel);
+  }
+  if (button10.fallingEdge()) {
+    usbMIDI.sendControlChange(10, cclow, channel);
+    if(isRec == 0) {
+      isRec = 1;
+      digitalWrite(0, HIGH);
+    } else {
+      isRec = 0;
+      digitalWrite(0, LOW);
+    }
+  }
+  if (button12.fallingEdge()) {
+    usbMIDI.sendControlChange(12, cclow, channel);
+  }
+
+  // Check each button for "rising" edge
+  if (button1.risingEdge()) {
+    usbMIDI.sendControlChange(1, cchigh, channel);
+  }
+  if (button2.risingEdge()) {
+    usbMIDI.sendControlChange(2, cchigh, channel);
+  }
+  if (button3.risingEdge()) {
+    usbMIDI.sendControlChange(3, cchigh, channel);
+  }
+  if (button4.risingEdge()) {
+    usbMIDI.sendControlChange(4, cchigh, channel);
+  }
+  if (button5.risingEdge()) {
+    usbMIDI.sendControlChange(5, cchigh, channel);
+  }
+  if (button6.risingEdge()) {
+    usbMIDI.sendControlChange(6, cchigh, channel);
+  }
+  if (button7.risingEdge()) {
+    usbMIDI.sendControlChange(14, cchigh, channel);
+  }
+  if (button8.risingEdge()) {
+    usbMIDI.sendControlChange(8, cchigh, channel);
+  }
+  if (button9.risingEdge()) {
+    usbMIDI.sendControlChange(9, cchigh, channel);
+  }
+  if (button10.risingEdge()) {
+    usbMIDI.sendControlChange(10, cchigh, channel);
+  }
+  if (button12.risingEdge()) {
+    usbMIDI.sendControlChange(12, cchigh, channel);
+  }
+
+
+  if(!countDownDone) {
+    DoCountdown();
+  } else {
+    if(isRec) {
+        line1 = "*REC*";
+        line2 = "*****";
+    } else if(isDub) {
+        line1 = "*DUB*";
+        line2 = "*****";
+    } else {
+      if(n14 >= 0 && n14 <= 10) {
+        line1 = "Loop";
+        line2 = "1";
+      } else if(n14 >= 11 && n14 <= 20) {
+        line1 = "Loop";
+        line2 = "2";
+      } else if(n14 >= 21 && n14 <= 30) {
+        line1 = "Loop";
+        line2 = "3";
+      } else if(n14 >= 31 && n14 <= 40) {
+        line1 = "Loop";
+        line2 = "4";
+      } else if(n14 >= 41 && n14 <= 50) {
+        line1 = "FX";
+        line2 = "1";
+      } else if(n14 >= 51 && n14 <= 60) {
+        line1 = "FX";
+        line2 = "2";
+      } else if(n14 >= 61 && n14 <= 70) {
+        line1 = "FX";
+        line2 = "3";
+      } else if(n14 >= 71 && n14 <= 80) {
+        line1 = "FX";
+        line2 = "4";
+      } else if(n14 >= 81 && n14 <= 90) {
+        line1 = "Generator";
+        line2 = "";
+      } else if(n14 >= 91 && n14 <= 100) {
+        line1 = "Utils";
+        line2 = "";
+      } else if(n14 >= 101 && n14 <= 110) {
+        line1 = "Samp-EQ";
+        line2 = "";
+      } else if(n14 >= 111 && n14 <= 120) {
+        line1 = "Mixer";
+        line2 = "";
+      } else if(n14 >= 121 && n14 <= 127) {
+      }
+    }
+    display.clearDisplay();
+    display.setTextSize(4);             // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(line1);
+    display.setCursor(0,36);             // Start at top-left corner
+    display.println(line2);
+    display.display();  
+  }
+  
+
+  // MIDI Controllers should discard incoming MIDI messages.
+  // http://forum.pjrc.com/threads/24179-Teensy-3-Ableton-Analog-CC-causes-midi-crash
+  while (usbMIDI.read()) {
+    // ignore incoming messages
+  }
+}
+
+
+void DoCountdown()
+{
+    display.clearDisplay();
+    static unsigned long
+        lastTimeNow = 0;
+    static byte
+        lastsecs = 1;
+       
+    timeNow = millis();
+    timeElapsed = timeNow - timeStart;
+   
+    if( mins == 0 && secs == 0 )
+        return;
+       
+    timeLeft = COUNTDOWN_TIME - timeElapsed;
+
+    mins = (byte)(timeLeft / 60000ul);
+    timeTemp = timeLeft - (mins * 60000);
+    secs = (byte)(timeTemp / 1000ul);
+    timeTemp = timeTemp - (secs * 1000ul);
+
+    if( mins == 0 && secs == 0 ) {
+        display.setTextSize(6);             // Normal 1:1 pixel scale
+        display.setTextColor(SSD1306_WHITE);        // Draw white text
+        display.setCursor(0,0);             // Start at top-left corner
+        display.println(F("JAS"));
+        display.display();
+        countDownDone = 1;
+    } else if( secs != lastsecs ) {
+        lastsecs = secs;
+        display.setTextSize(4);
+        display.setCursor(0,0);
+        sprintf( szString, "%02d:%02d", mins, secs );
+        //display.setCursor( 50, 30 );
+        display.print( szString );
+        display.setCursor(0,36);             // Start at top-left corner
+        display.println("<- OFF");
+
+        display.display();
+    }
+}
