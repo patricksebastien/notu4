2ba0355f4195d470a88cb2dcf997a40f89e7629a
Patrick <patrick@agora.studio>
2020-12-06 23:15:37 -0500

    do not send for X us

--- notforu_esp32_pedal/notforu/esp32/main/main_withmidiout.cpp
+++ notforu_esp32_pedal/notforu/esp32/main/main_withmidiout.cpp
@@ -14,6 +14,7 @@
 #include "freertos/queue.h"
 #include <nvs_flash.h>
 #include <protocol_examples_common.h>
+#include "driver/uart.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "esp_timer.h"
@@ -45,36 +46,36 @@ int ip_protocol;
 struct sockaddr_in dest_addr;
 int sock;
 
+#define LED GPIO_NUM_2
 #define PRINT_LINK_STATE false
 
+// Serial midi
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_TXD  (GPIO_NUM_4)
+#define ECHO_TEST_RXD  (GPIO_NUM_5)
+#define BUF_SIZE (1024)
+#define MIDI_TIMING_CLOCK 0xF8
+#define MIDI_START 0xFA
+#define MIDI_STOP 0xFC
+#define MIDI_SONG_POSITION_POINTER 0xF2
+
 // Global
+static void periodic_timer_callback(void* arg);
+esp_timer_handle_t periodic_timer;
 bool startStopCB = false;
 bool startStopState = false;
 double curr_beat_time;
 double prev_beat_time;
 
-unsigned long debounce_us = 1000000; // not a real debounce, just send a quickly as possible and not send the event for X microseconds
-time_t in0;
-time_t in1;
-time_t in2;
-time_t in3;
-time_t in4;
-time_t in5;
-time_t in6;
-time_t in7;
-time_t in8;
-time_t in9;
-
-
-
 // gpio
 extern "C" {
   
   // BUTTONS
-  #define GPIO_INPUT_IO_0     GPIO_NUM_36 // no pull-up
-  #define GPIO_INPUT_IO_1     GPIO_NUM_39 // no pull-up
-  #define GPIO_INPUT_IO_2     GPIO_NUM_34 // no pull-up
-  #define GPIO_INPUT_IO_3     GPIO_NUM_35 // no pull-up
+  #define GPIO_INPUT_IO_0     GPIO_NUM_36
+  #define GPIO_INPUT_IO_1     GPIO_NUM_39
+  #define GPIO_INPUT_IO_2     GPIO_NUM_34
+  #define GPIO_INPUT_IO_3     GPIO_NUM_35
   #define GPIO_INPUT_IO_4     GPIO_NUM_32
   #define GPIO_INPUT_IO_5     GPIO_NUM_33
   #define GPIO_INPUT_IO_6     GPIO_NUM_25
@@ -112,58 +113,39 @@ extern "C" {
       uint32_t io_num;
       for(;;) {
           if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+              /*
               // check gpio_input for low
               // debounce in pd...
-              if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in0) > debounce_us) {
-                in0 = esp_timer_get_time();
+              if(io_num == GPIO_NUM_36 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "0";
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in1) > debounce_us) {
-                in1 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_39 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "1";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in2) > debounce_us) {
-                in2 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_34 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "2";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in3) > debounce_us) {
-                in3 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_35 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "3";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in4) > debounce_us) {
-                in4 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "4";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_33 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in5) > debounce_us) {
-                in5 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_33 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "5";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in6) > debounce_us) {
-                in6 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_25 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "6";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in7) > debounce_us) {
-                in7 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_26 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "7";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in8) > debounce_us) {
-                in8 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_27 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "8";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-              } else if((io_num == GPIO_NUM_32 && gpio_get_level((gpio_num_t)io_num) == 0) && (esp_timer_get_time() - in9) > debounce_us) {
-                in9 = esp_timer_get_time();
+              } else if(io_num == GPIO_NUM_14 && gpio_get_level((gpio_num_t)io_num) == 0) {
                 payload = "9";
-                sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
               }
 
-              /*
-              int err = 
-              if (err < 0) {
-                  ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                  break;
-              }
-              ESP_LOGI(TAG, "Message sent");
-            
-              printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level((gpio_num_t)io_num));*/
+              int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+                  if (err < 0) {
+                      ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                      break;
+                  }
+                  ESP_LOGI(TAG, "Message sent");
+                
+                printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level((gpio_num_t)io_num));*/
           }
       }
   }
@@ -230,6 +212,13 @@ void printTask(void* userParam)
   }
 }
 
+// callbacks
+void tempoChanged(double tempo) {
+    double midiClockMicroSecond = ((60000 / tempo) / 24) * 1000;
+    esp_timer_handle_t periodic_timer_handle = (esp_timer_handle_t) periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_stop(periodic_timer_handle));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer_handle, midiClockMicroSecond));
+}
 
 void startStopChanged(bool state) {
   // received as soon as sent
@@ -245,6 +234,7 @@ void tickTask(void* userParam)
   link.enableStartStopSync(true); // if not no callback for start/stop
 
   // callbacks
+  link.setTempoCallback(tempoChanged);
   link.setStartStopCallback(startStopChanged);
 
   // debug
@@ -252,7 +242,7 @@ void tickTask(void* userParam)
   {
     xTaskCreate(printTask, "print", 8192, &link, 1, nullptr);
   }
-
+  //gpio_set_direction(LED, GPIO_MODE_OUTPUT);
 
   // phase
   while (true)
@@ -260,7 +250,7 @@ void tickTask(void* userParam)
     xSemaphoreTake(userParam, portMAX_DELAY);
 
     const auto state = link.captureAudioSessionState();
-    //const auto phase = state.phaseAtTime(link.clock().micros(), 0.25);
+    const auto phase = state.phaseAtTime(link.clock().micros(), 0.25);
     //gpio_set_level(LED, fmodf(phase, 1.) < 0.1);
    
     curr_beat_time = state.beatAtTime(link.clock().micros(), 4);
@@ -295,13 +285,30 @@ void tickTask(void* userParam)
             gpio_set_level(GPIO_OUTPUT_IO_3, 0);
           }
         //}
-       
+
+        if(curr_step == 0 && startStopState != startStopCB) {
+          char zedata[] = { MIDI_START };
+          uart_write_bytes(UART_NUM_1, zedata, 1);
+          uart_write_bytes(UART_NUM_1, 0, 1);
+        } else {
+          char zedata[] = { MIDI_STOP };
+          uart_write_bytes(UART_NUM_1, zedata, 1);
+          uart_write_bytes(UART_NUM_1, 0, 1);
+        }
+        startStopState = startStopCB;
+        
       }
     }
     prev_beat_time = curr_beat_time;
 
     portYIELD();
   }
+}
+
+static void periodic_timer_callback(void* arg)
+{
+    char zedata[] = { MIDI_TIMING_CLOCK };
+    uart_write_bytes(UART_NUM_1, zedata, 1);
 }
 
 
@@ -334,11 +341,6 @@ static void udp_client_task(void *pvParameters)
                 rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
                 ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
                 ESP_LOGI(TAG, "%s", rx_buffer);
-                /*if(rx_buffer == 1) {
-                  gpio_set_level(GPIO_OUTPUT_IO_8, 0);
-                } else {
-                  gpio_set_level(GPIO_OUTPUT_IO_8, 1);
-                }*/
             }
 
             vTaskDelay(100 / portTICK_PERIOD_MS);
@@ -360,6 +362,19 @@ extern "C" void app_main()
   ESP_ERROR_CHECK(esp_event_loop_create_default());
   ESP_ERROR_CHECK(example_connect());
 
+  // serial
+  uart_config_t uart_config = {
+    .baud_rate = 31250, // midi speed
+    .data_bits = UART_DATA_8_BITS,
+    .parity    = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    .rx_flow_ctrl_thresh = 122,
+  };
+  uart_param_config(UART_NUM_1, &uart_config);
+  uart_set_pin(UART_NUM_1, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+  uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
+
   // upd init + timer
   dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
   dest_addr.sin_family = AF_INET;
@@ -378,6 +393,59 @@ extern "C" void app_main()
   SemaphoreHandle_t tickSemphr = xSemaphoreCreateBinary();
   timerGroup0Init(1000, tickSemphr);
   xTaskCreate(tickTask, "tick", 8192, tickSemphr, 1, nullptr);
+
+  // midi clock
+  const esp_timer_create_args_t periodic_timer_args = {
+          .callback = &periodic_timer_callback,
+          .name = "periodic"
+  };
+  ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+  ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 20833.333333333)); // 120 bpm by default
+
+
+/*
+    //disable interrupt
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode    
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio intrrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+
+    //remove isr handler for gpio number.
+    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);*/
 
   // LED
   gpio_config_t io_conf;
